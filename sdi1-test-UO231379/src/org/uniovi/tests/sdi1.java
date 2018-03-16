ab708da9dc075174d2849b3b213d764e40ae4999
NachoMartin123 <nacho.mf95@gmail.com>
2018-03-16 02:55:02 +0100

    tests

--- sdi1-test-UO231379/src/org/uniovi/tests/sdi1.java
+++ sdi1-test-UO231379/src/org/uniovi/tests/sdi1.java
@@ -20,6 +20,7 @@ import com.uniovi.tests.pageobjects.PO_LoginView;
 import com.uniovi.tests.pageobjects.PO_RegisterPublicationView;
 import com.uniovi.tests.pageobjects.PO_Properties;
 import com.uniovi.tests.pageobjects.PO_RegisterView;
+import com.uniovi.tests.pageobjects.PO_UserListView;
 import com.uniovi.tests.pageobjects.PO_View;
 import com.uniovi.tests.util.SeleniumUtils;
 
@@ -28,7 +29,8 @@ public class sdi1 {
 
 	// En Windows (Debe ser la versión 46.0 y desactivar las actualizacioens
 	// automáticas)):
-	static String PathFirefox = "C:\\Users\\Blacky\\Desktop\\PEQUE\\CURSO 2017-2018\\SDI\\PRACTICAS\\SESION_5\\Firefox46.0.win\\Firefox46.win\\FirefoxPortable.exe";
+//	static String PathFirefox = "C:\\Users\\Blacky\\Desktop\\PEQUE\\CURSO 2017-2018\\SDI\\PRACTICAS\\SESION_5\\Firefox46.0.win\\Firefox46.win\\FirefoxPortable.exe";
+	static String PathFirefox = "C:\\Users\\Nacho\\Desktop\\Firefox46.win\\FirefoxPortable.exe";
 	// //En MACOSX (Debe ser la versión 46.0 y desactivar las actualizaciones
 	// automáticas):
 	// static String PathFirefox =
@@ -129,9 +131,10 @@ public class sdi1 {
 	}
 	
 	// PR3.1[LisUsrInVal] Acceso al listado de usuarios desde un usuario no autenticado
-		@Test
-		public void LisUsrInVal() {
-		}
+	@Test
+	public void LisUsrInVal() {
+		assertFalse(1>0);
+	}
 	
 	// PR4.1[BusUsrVal] Busqueda valida en listado de usuarios desde usuario en sesion
 	@Test
@@ -170,36 +173,56 @@ public class sdi1 {
 
 	// PR5.1[InvVal] Enviar una invitación de amistad a un usuario de forma valida.
 	@Test
-	public void InvVal() {
-
+	public void InvVal() {//NACHO
+		//Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "login", "id", "btnIdentificate");
+		// Rellenamos el formulario.
+		PO_LoginView.fillForm(driver, "pedro@gmail.com", "123456");
+		//Pinchamos en la opción de menu de ver usuarios: 
+		PO_HomeView.clickOption(driver, "/user/list", "text", "Usuarios");
+		assertEquals("Agregar amigo",PO_UserListView.getButtonText(driver, "resendButton7"));
+		PO_UserListView.clickButton(driver, "resendButton7");
 	}
 
 	// PR5.2[InvInVal] Enviar una invitación de amistad a un usuario al que ya le habíamos
 	//invitado la invitación previamente. No debería dejarnos enviar la invitación,
 	//se podría ocultar el botón de enviar invitación o notificar que ya había sido enviada previamente.
 	@Test
-	public void InvInVal() {
-
+	public void InvInVal() {//NACHO
+		//Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "login", "id", "btnIdentificate");
+		// Rellenamos el formulario.
+		PO_LoginView.fillForm(driver, "pedro@gmail.com", "123456");
+		//Pinchamos en la opción de menu de ver usuarios: 
+		PO_HomeView.clickOption(driver, "/user/list", "text", "Usuarios");
+		PO_UserListView.clickButton(driver, "noresendButton4");
+		assertEquals("Petición enviada",PO_UserListView.getButtonText(driver, "noresendButton4"));
 	}
 	
 	// PR6.1 [LisInvVal] Listar las invitaciones recibidas por un usuario, realizar la comprobación
 	//con una lista que al menos tenga una invitación recibida.
 	@Test
-	public void LisInvVal() {
-
+	public void LisInvVal() {//NACHO
+		//Vamos al formulario de registro
+//		PO_HomeView.clickOption(driver, "login", "id", "btnIdentificate");
+//		// Rellenamos el formulario.
+//		PO_LoginView.fillForm(driver, "pedro@gmail.com", "123456");
+//		//Pinchamos en la opción de menu de ver usuarios: 
+//		PO_HomeView.clickOption(driver, "Peticiones recibidas", "text", "Listado de invitaciones recibidas:");
+		assertFalse(1>0);
 	}
 	
 	// PR7.1 [AcepInvVal] Aceptar una invitación recibida.
 	@Test
 	public void AcepInvVal() {
-
+		assertFalse(1>0);
 	}
 	
 	// PR8.1 [ListAmiVal] Listar los amigos de un usuario, realizar la comprobación con 
 	//una lista que al menos tenga un amigo.
 	@Test
 	public void ListAmiVal() {
-
+		assertFalse(1>0);
 	}
 	
 	// PR9.1 [PubVal] Crear una publicación con datos válidos.
@@ -244,59 +267,59 @@ public class sdi1 {
 	// PR11.1 [LisPubAmiVal] Listar las publicaciones de un usuario amigo
 	@Test
 	public void LisPubAmiVal() {
-
+		assertFalse(1>0);
 	}
 	
 	// PR11.2 [LisPubAmiInVal] Utilizando un acceso vía URL tratar de listar 
 	//las publicaciones de un usuario que no sea amigo del usuario identificado en sesión.
 	@Test
 	public void LisPubAmiInVal() {
-
+		assertFalse(1>0);
 	}
 	
 	// PR12.1 [PubFot1Val] Crear una publicación con datos válidos y una foto adjunta.
 	@Test
 	public void PubFot1Val() {
-
+		assertFalse(1>0);
 	}
 	
 	// PR12.1 [PubFot2Val] Crear una publicación con datos válidos y sin una foto adjunta.
 	@Test
 	public void PubFot2Val() {
-
+		assertFalse(1>0);
 	}
 	
 	// PR13.1 [AdInVal] Inicio de sesión como administrador con datos válidos.
 	@Test
 	public void AdInVal() {
-
+		assertFalse(1>0);
 	}
 	
 	// PR13.2 [AdInInVal] Inicio de sesión como administrador con datos inválidos (usar los datos 
 	//de un usuario que no tenga perfil administrador).
 	@Test
 	public void AdInInVal() {
-
+		assertFalse(1>0);
 	}
 	
 	// PR14.1 [AdLisUsrVal] Desde un usuario identificado en sesión como administrador listar a 
 	//todos los usuarios de la aplicación.
 	@Test
 	public void AdLisUsrVal() {
-
+		assertFalse(1>0);
 	}
 	
 	// PR15.1 [AdBorUsrVal] Desde un usuario identificado en sesión como administrador eliminar un 
 	//usuario existente en la aplicación.
 	@Test
 	public void AdBorUsrVal() {
-
+		assertFalse(1>0);
 	}
 
 	// PR15.2 [AdBorUsrInVal] Intento de acceso vía URL al borrado de un usuario existente en la aplicación. 
 	//Debe utilizarse un usuario identificado en sesión pero que no tenga perfil de administrador.
 	@Test
 	public void AdBorUsrInVal() {
-
+		assertFalse(1>0);
 	}
 }
